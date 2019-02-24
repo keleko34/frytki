7ba5c6f2d815320c75291db9284008b2a543fa5c
keleko34 <Gregory.Guidero@gmail.com>
2019-02-24 12:02:55 +0100

    Rename main package from init to frytki so that it shows up better in the dev console

--- build.js
+++ build.js
@@ -5,9 +5,9 @@ var base = process.cwd().replace(/\\/g,'/'),
 
 console.log("Building Library...");
 
-flags.jsCode = [{src: fs.readFileSync(base+'/init.js','utf8')}];
+flags.jsCode = [{src: fs.readFileSync(base+'/frytki.js','utf8')}];
 flags.compilationLevel = 'SIMPLE';
 
-fs.writeFileSync(base+'/init.min.js',closureCompiler(flags).compiledCode);
+fs.writeFileSync(base+'/frytki.min.js',closureCompiler(flags).compiledCode);
 
 console.log("Finished Building Minified Library..");
